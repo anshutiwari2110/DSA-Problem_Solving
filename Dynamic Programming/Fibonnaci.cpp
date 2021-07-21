ddaaedc0b717c1102b916e52bd7dada7b7b4e66f
anshu <anshubullettiwari@gmail.com>
2021-07-21 21:44:00 +0530

    First Commit

---
+++ Dynamic Programming/Fibonnaci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int fib(int n){
+    vector<long long int> f(n+2,0);
+    f[0] = 0;
+    f[1] = 1;
+
+    for(int i = 2;i<=n;i++){
+        f[i] = f[i-2] + f[i-1];
+    }
+
+    return f[n];
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+    return 0;
+}
